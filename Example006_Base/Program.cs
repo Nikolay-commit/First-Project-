ed7a0631224a24ab551e21d171814e36855bbfda
Nikolay <nikolay-ko@mail.ru>
2023-04-19 21:06:46 +0300

    Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 10;
+int b = 21;
+int c = 65;
+int d = 89;
+int e = 405;
+
+int max = a;
+
+if(a > max ) max = a; 
+if(b > max ) max = b;
+if(c > max ) max = c;
+if(d > max ) max = d;
+if(e > max ) max = e;
+
+Console.WriteLine("max =");
+Console.WriteLine(max);
+
