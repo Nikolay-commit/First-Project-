a4e4fadf52078998364d4c65f0388f0c6d57ee61
Nikolay <nikolay-ko@mail.ru>
2023-04-19 20:49:15 +0300

    Example005 if elso

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "марина")
+{
+    Console.WriteLine("Ура, это же МАРИНЭ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
